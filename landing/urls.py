55b3a8a7945defe6e3770419aba2e32d459b0bd8
Ajawu David <ajawudavid@gmail.com>
2020-10-28 01:28:50 +0100

    added home and about template

--- landing/urls.py
+++ landing/urls.py
@@ -3,4 +3,9 @@ from landing import views
 
 urlpatterns = [
     path('', views.LandingPageView.as_view(), name='landing_page'),
+    path('about/', views.AboutView.as_view(), name='about_page'),
+    path('services/', views.AboutView.as_view(), name='services_page'),
+    path('equipments/', views.AboutView.as_view(), name='equipments_page'),
+    path('contact/', views.AboutView.as_view(), name='contact_page'),
+    path('quote/', views.AboutView.as_view(), name='quote_page'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,12 +1,17 @@
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from .models import Contact
 from .forms import ContactForm
 from .tasks import send_email_task
 
 
-class LandingPageView(CreateView):
+class LandingPageView(TemplateView):
+    template_name = 'landing/home.html'
+
+
+class ContactPageView(CreateView):
     model = Contact
-    template_name = 'landing/landing_page.html'
+    template_name = 'landing/home.html'
     form_class = ContactForm
     success_url = '/'
 
@@ -14,3 +19,7 @@ class LandingPageView(CreateView):
         self.object = form.save()
         send_email_task.delay(self.object.id)
         return super().form_valid(form)
+
+
+class AboutView(TemplateView):
+    template_name = 'landing/about.html'
